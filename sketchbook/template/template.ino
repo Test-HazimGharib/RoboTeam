f81c48cf4b594ba395bfddafae3e7161d64ede64
hazimgharib <hazimgharib21@gmail.com>
2017-04-29 10:10:34 +0000

    Add template.ino

---
+++ sketchbook/template/template.ino
@@ -0,0 +1,22 @@
+//  Library
+//    Put your libraries that you want to use here
+
+//  Define
+//    Put your constant definition here
+
+//  Variable
+//    Put your variables here
+
+//  Extras
+//    Put your extra setup here, ex. library initialization
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
